67007d0101a40e6c42bdc955cbbad9b2eb522760
Mustkass <mustkass@noreply.01.kood.tech>
2023-02-25 01:11:04 +0000

    The function to create arrays of slices from the source file.

--- mapStructure.go
+++ mapStructure.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func main() {
+func CreateFarmStruct() {
 	TunnelList := []string{} // The slice array of tunnels
 	RoomList := []string{}   // The slice array of room names and coordinates
 	var NumberOfAnts int     // The number of ants from the source file
@@ -31,9 +31,11 @@ func main() {
 			}
 			// Need to add check for int coordinates
 			if (len(scanner.Text()) > 0) && (r == 32) { // Searches for rooms & coordinates
-				fmt.Println(scanner.Text())                 // Prints room coordinates to the terminal
-				RoomList = append(RoomList, scanner.Text()) // Ready list of rooms with coordinates made as a string array
-				break
+				if CheckRoomList(scanner.Text()) == true { // Checks if the string with spaces contains the room name and coordinates
+					fmt.Println(scanner.Text())                 // Prints room coordinates to the terminal
+					RoomList = append(RoomList, scanner.Text()) // Ready list of rooms with coordinates made as a string array
+					break
+				}
 			}
 			i, err := strconv.Atoi(scanner.Text()) // If there is only one number in the line - according to the rules this can be only the number of ants
 			if err == nil {
@@ -46,4 +48,27 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func CheckRoomList(a string) bool {
+	var aa string
+	var RoomName string
+	wasSpace := false
+	for i := 0; i < len(a)-1; i++ {
+		if a[0] != 76 && a[0] != 35 && a[i] != 32 && wasSpace == false {
+			RoomName = RoomName + string(a[i])
+		}
+		if a[i] == 32 && wasSpace == false {
+			wasSpace = true // Checks only the string after the first space because room name can be not int
+		}
+		if a[i] != 32 && wasSpace == true {
+			aa = string(a[i])
+			_, err := strconv.Atoi(aa)
+			if err != nil {
+				return false
+			}
+		}
+	}
+	fmt.Println("Room name:", RoomName)
+	return true
 }
