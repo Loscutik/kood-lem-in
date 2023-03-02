d98ba275f9e0aa348751b7b75aa468c1947983c0
Mustkass <mustkass@noreply.01.kood.tech>
2023-03-02 16:10:28 +0000

    Creates the required structure, in progress...

--- mapStructure.go
+++ mapStructure.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func CreateFarmStruct() {
+type AntFarm struct {
+	Rooms      []*Room
+	Start, End *Room
+}
+type Room struct {
+	Name  string   // Done
+	Links []string // Done
+	x, y  int      // coordinates
+}
+
+func (r *Room) CreateFarmStruct() {
 	TunnelList := []string{} // The slice array of tunnels
 	RoomList := []string{}   // The slice array of room names and coordinates
 	var NumberOfAnts int     // The number of ants from the source file
@@ -23,15 +34,14 @@ func CreateFarmStruct() {
 	count := 0                     // Counts the lines.
 	for scanner.Scan() {           // This scanner gets the file content line by line
 		count++
-		for _, r := range scanner.Text() {
-			if (len(scanner.Text()) == 3) && (r == 45) { // Searches for tunnel pairs
+		for _, h := range scanner.Text() {
+			if (len(scanner.Text()) == 3) && (h == 45) { // Searches for tunnel pairs
 				fmt.Println(scanner.Text())                     // Prints tunnel pairs to the terminal
 				TunnelList = append(TunnelList, scanner.Text()) // Ready list of tunnels made as a string array
 				break
 			}
-			// Need to add check for int coordinates
-			if (len(scanner.Text()) > 0) && (r == 32) { // Searches for rooms & coordinates
-				if CheckRoomList(scanner.Text()) == true { // Checks if the string with spaces contains the room name and coordinates
+			if (len(scanner.Text()) > 0) && (h == 32) { // Searches for rooms & coordinates
+				if r.CheckRoomList(scanner.Text()) { // Checks if the string with spaces contains the room name and coordinates
 					fmt.Println(scanner.Text())                 // Prints room coordinates to the terminal
 					RoomList = append(RoomList, scanner.Text()) // Ready list of rooms with coordinates made as a string array
 					break
@@ -48,27 +58,47 @@ func CreateFarmStruct() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Name:", r.Name)
+	//fmt.Println("Coordinates:", r.x, r.y)
+	r.Links = TunnelList
+	fmt.Println("Tunnels:", r.Links)
 }
 
-func CheckRoomList(a string) bool {
-	var aa string
-	var RoomName string
-	wasSpace := false
-	for i := 0; i < len(a)-1; i++ {
-		if a[0] != 76 && a[0] != 35 && a[i] != 32 && wasSpace == false {
-			RoomName = RoomName + string(a[i])
+func (r *Room) CheckRoomList(a string) bool {
+	StringArray := strings.Fields(a)
+	for h, k := range StringArray {
+		if len(StringArray) > 3 { // Checking if there are 3 parts of the string - Name, x and y
+			return false
 		}
-		if a[i] == 32 && wasSpace == false {
-			wasSpace = true // Checks only the string after the first space because room name can be not int
-		}
-		if a[i] != 32 && wasSpace == true {
-			aa = string(a[i])
-			_, err := strconv.Atoi(aa)
+		switch h {
+		case 0: // The first part - the name of the room
+			for i := 0; i < len(k); i++ {
+				if k[0] != 76 && k[0] != 35 {
+					r.Name = r.Name + k + " " // NB, NEEDS TO BE CONVERTED TO MAP
+					break
+				} else {
+					return false
+				}
+			}
+		case 1: // The second part - coordinates, x
+			j, err := strconv.Atoi(k) // Checking if it is int
+			if err == nil {
+				r.x = j
+			}
 			if err != nil {
 				return false
 			}
+			fmt.Println("X:", k)
+		case 2: // The third part - coordinates, y
+			j, err := strconv.Atoi(k) // Checking if it is int
+			if err == nil {
+				r.y = j
+			}
+			if err != nil {
+				return false
+			}
+			fmt.Println("Y:", k)
 		}
 	}
-	fmt.Println("Room name:", RoomName)
 	return true
 }
